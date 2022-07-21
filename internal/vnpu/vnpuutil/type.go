a817d5d09c10b1297f97877c78e79ecd9369f80a
BianTanggui <zhangxiaopeng24@huawei.com>
2022-07-21 10:37:26 +0800

    Match-id-bba0470f5f2f3f301bd5f912bbdcc67f3d0f9eea

--- internal/vnpu/vnpuutil/type.go
+++ internal/vnpu/vnpuutil/type.go
@@ -17,19 +17,10 @@ import (
 const (
 	// PluginName the vNPU's plugin name.
 	PluginName = "Vnpu"
-	// NPUCardNamePrefix huawei.com/ for judge npu resource.
-	NPUCardNamePrefix = "huawei.com/"
 	// NPUIdentifyName to identify the NPU
 	NPUIdentifyName = util.CommCardPreName
-	// NPU910CardName for judge 910 npu resource.
-	NPU910CardName = "huawei.com/Ascend910"
 	// NPU310PCardName for judge 310P npu resource.
 	NPU310PCardName = "huawei.com/Ascend310P"
-	// NPU910CardCoreKey for npu card core. like chipId-allCores-freeCores example:4-32c-4c
-	NPU910CardCoreKey = "huawei.com/Ascend910-spec"
-
-	// VNPCMDataKey cm date key
-	VNPCMDataKey = "VNPUCfg"
 )
 
 // ComVNPU common type
